3b53dfda517fbea1b63a2c590f38f97a1d38738f
Takara Hokao <takara.hokao@gmail.com>
2013-12-05 23:23:00 +0900

    unregister メソッドを追加

--- ClassLoader.php
+++ ClassLoader.php
@@ -2,12 +2,33 @@
 
 class ClassLoader {
     protected $dirs;
+
+    /**
+     * クラスを登録する
+     **/
     public function register() {
         spl_autoload_register(array($this, 'loadClass'));
     }
+
+    /**
+     * クラスを解除する
+     **/
+    public function unregister() {
+        spl_autoload_unregister(array($this,'loadClass'));
+    }
+
+    /**
+     *  ディレクトリ登録する
+     * @param string $dir
+     **/
     public function registerDir($dir) {
         $this->dirs[] = $dir;
     }
+
+    /**
+     * auto load classes
+     * @param string $class
+     **/
     public function loadClass($class) {
         foreach ($this->dirs as $dir) {
             $file = $dir . '/' . $class . '.php';
